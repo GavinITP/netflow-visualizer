9362d3d255762a6d95fd8132ea456f12d6e6432b
GavinITP <86824319+GavinITP@users.noreply.github.com>
2025-03-25 20:47:11 +0700

    feat: creat layout with sticky sidebar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,11 +5,27 @@
 </script>
 
 <div class="flex min-h-screen bg-gray-100">
-  <div>
-    <div class="min-h-full w-[250px] bg-red-500">test sidebar</div>
-  </div>
+  <aside class="w-[90px] p-4 lg:w-[250px]">
+    <div
+      class="sticky top-4 min-h-[calc(100vh-32px)] rounded-lg bg-white p-6 shadow-lg"
+    >
+      <h2 class="mb-4 text-xl font-bold">Sidebar</h2>
 
-  <main class="flex-1">
+      <ul>
+        <li class="mb-2">
+          <a href="#" class="">1</a>
+        </li>
+        <li class="mb-2">
+          <a href="#" class="">2</a>
+        </li>
+        <li class="mb-2">
+          <a href="#" class="">3</a>
+        </li>
+      </ul>
+    </div>
+  </aside>
+
+  <main class="flex-1 p-4">
     {@render children()}
   </main>
 </div>
