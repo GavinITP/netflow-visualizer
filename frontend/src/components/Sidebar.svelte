be2da7bbd64582ef7a44f19dc4c3e0cd43dd399b
GavinITP <86824319+GavinITP@users.noreply.github.com>
2025-03-25 22:30:53 +0700

    style: add responsive transition

--- frontend/src/components/Sidebar.svelte
+++ frontend/src/components/Sidebar.svelte
@@ -16,15 +16,17 @@
   ];
 </script>
 
-<aside class="w-[90px] p-4 lg:w-[280px]">
+<aside
+  class="w-[90px] p-4 transition-all duration-300 ease-in-out lg:w-[280px]"
+>
   <div
     class="sticky top-4 min-h-[calc(100vh-32px)] rounded-lg bg-white shadow-md"
   >
     <div
-      class="flex items-center justify-center gap-2 px-1 lg:justify-start lg:px-6"
+      class="flex h-16 items-center justify-center gap-2 px-1 lg:justify-start lg:px-6"
     >
-      <img src="/logo.svg" alt="logo" class="my-4 h-[27px] w-[27px]" />
-      <h2 class="my-4 hidden text-2xl font-bold lg:block">StreamFlow</h2>
+      <img src="/logo.svg" alt="logo" class="h-7 w-7" />
+      <h2 class="hidden text-2xl font-bold lg:block">StreamFlow</h2>
     </div>
 
     <div class="h-[1px] w-full bg-[#E8E8E8]"></div>
