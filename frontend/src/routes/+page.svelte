729894778540de9c92d2a1854b4ad9b6e30fbb78
GavinITP <86824319+GavinITP@users.noreply.github.com>
2025-03-25 20:12:18 +0700

    feat(font): add local fonts for Roboto and Roboto Condensed

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<p class="font-extrabold">Netflow website</p>
+<h1 class="text-4xl font-extrabold">Netflow Website</h1>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque mollitiinventore optio eligendi
+	eaque vero cupiditate ipsa perferendis, dolores delectus consectetur earum repudiandae corrupti
+	ipsum rerum non fugit doloremque distinctio.
+</p>
