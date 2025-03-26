d2a74be4e4d9cd1545c60004dbc0e65a1a0181bc
GavinITP <86824319+GavinITP@users.noreply.github.com>
2025-03-26 22:55:53 +0700

    fix: chart type and font display

--- frontend/src/components/ProtocolChart.svelte
+++ frontend/src/components/ProtocolChart.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { onMount } from "svelte";
   import {
     Chart,
@@ -9,43 +9,45 @@
     Title,
   } from "chart.js";
 
-  let canvas;
+  let canvas: HTMLCanvasElement;
 
   onMount(() => {
     Chart.register(PieController, ArcElement, Tooltip, Legend, Title);
 
     const ctx = canvas.getContext("2d");
 
-    const chart = new Chart(ctx, {
-      type: "pie",
-      data: {
-        labels: ["TCP", "UDP"],
-        datasets: [
-          {
-            data: [29, 71],
-            backgroundColor: ["#A593E0", "#D6C1F0"], // Violet pastel colors
-            borderColor: "#fff",
-            borderWidth: 2,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            font: {
-              size: 18,
+    if (ctx) {
+      const chart = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: ["TCP", "UDP"],
+          datasets: [
+            {
+              data: [29, 71],
+              backgroundColor: ["#A593E0", "#D6C1F0"], // Violet pastel colors
+              borderColor: "#fff",
+              borderWidth: 2,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              font: {
+                size: 18,
+              },
+            },
+            legend: {
+              position: "bottom",
             },
           },
-          legend: {
-            position: "bottom",
-          },
         },
-      },
-    });
+      });
 
-    return () => chart.destroy();
+      return () => chart.destroy();
+    }
   });
 </script>
 
