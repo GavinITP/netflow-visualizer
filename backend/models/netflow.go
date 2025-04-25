86b515838f4f8c51311017bb156a8ce3a6a03d2c
GavinITP <86824319+GavinITP@users.noreply.github.com>
2025-04-25 16:20:51 +0700

    fix: change netflow schema

--- backend/models/netflow.go
+++ backend/models/netflow.go
@@ -2,16 +2,32 @@ package models
 
 import "time"
 
-type Netflow struct {
-	ID        uint      `json:"id"`        // CSV field: id
-	Timestamp time.Time `json:"timestamp"` // CSV field: timestamp
-	SrcAddr   string    `json:"srcaddr"`   // CSV field: srcaddr
-	DstAddr   string    `json:"dstaddr"`   // CSV field: dstaddr
-	NextHop   string    `json:"nexthop"`   // CSV field: nexthop
-	DPkts     uint64    `json:"dPkts"`     // CSV field: dPkts
-	DOctets   uint64    `json:"dOctets"`   // CSV field: dOctets
-	SrcPort   int       `json:"srcport"`   // CSV field: srcport
-	DstPort   int       `json:"dstport"`   // CSV field: dstport
-	Prot      string    `json:"prot"`      // CSV field: prot
-	Tos       int       `json:"tos"`       // CSV field: tos
+type BaseNetflow struct {
+	SrcAddr string `json:"srcaddr"`
+	DstAddr string `json:"dstaddr"`
+	NextHop string `json:"nexthop"`
+	DPkts   uint64 `json:"dPkts"`
+	DOctets uint64 `json:"dOctets"`
+	SrcPort int    `json:"srcport"`
+	DstPort int    `json:"dstport"`
+	Prot    string `json:"prot"`
+	Tos     int    `json:"tos"`
+}
+
+type NormalNetflow struct {
+	BaseNetflow
+	First time.Time `json:"first"`
+}
+
+type AnomalyNetflow struct {
+	BaseNetflow
+	Input    int       `json:"input"`
+	Output   int       `json:"output"`
+	First    time.Time `json:"first"`
+	Last     time.Time `json:"last"`
+	TCPFlags string    `json:"tcp_flags"`
+	SrcAS    int       `json:"src_as"`
+	DstAS    int       `json:"dst_as"`
+	SrcMask  int       `json:"src_mask"`
+	DstMask  int       `json:"dst_mask"`
 }
